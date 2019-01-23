4dd69c3401f0859b280034f1a4350ca81a5f4488
732153 <c0732153@gmail.com>
2019-01-23 11:54:31 -0500

    hhhhh

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            int i = 7;
+            int Sum;
+            Sum = 0;
+            while (i  <= 12)
+            {
+                Sum += i;
+                i++;
+                
+            }
+            Console.WriteLine(" Sum = {0}", Sum);
+            Console.ReadLine();
+
         }
     }
 }
